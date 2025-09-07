feda8b6c09c7e9c63a059f3cfcdf734af98bc9f9
gyankrishna-gk <gyankrishna.gk@gmail.com>
2025-09-07 07:37:30 +0530

    add

---
+++ src/day1/sumoftwonumbers.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class sumoftwonumbers {
+    public static void main(String[] args) {
+        int a1=0;
+        int a2=2;
+        int a3=a1+a2;
+        System.out.println(a3);
+    }
+}
